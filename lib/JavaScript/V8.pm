0ae17ea72b044d9645b486b52b63522ecfbc7455
Paweł Murias <pawelmurias@gmail.com>
2009-12-30 13:06:24 +0000

    Import of PMURIAS/JavaScript-V8-0.02 from CPAN.
    gitpan-cpan-distribution: JavaScript-V8
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         PMURIAS/JavaScript-V8-0.02.tar.gz
    gitpan-cpan-author:       PMURIAS
    gitpan-cpan-maturity:     released

--- lib/JavaScript/V8.pm
+++ lib/JavaScript/V8.pm
@@ -3,7 +3,7 @@ use v5.10;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 require XSLoader;
 XSLoader::load('JavaScript::V8', $VERSION);
@@ -39,15 +39,17 @@ JavaScript::V8 - Perl interface to the V8
 
 =head1 INSTALLING V8
 
+See http://code.google.com/p/v8/issues/detail?id=413 if you are having trouble on gcc 4.4.1
+
     svn checkout http://v8.googlecode.com/svn/trunk/ v8
-
     cd v8
-
     scons 
-
     sudo mv include/v8.h /usr/local/include/
-
     sudo mv libv8.a /usr/local/lib/
+
+=head1 REPOSITORY
+
+The source code lives at http://github.com/pmurias/javascript-v8.
 
 =head1 AUTHORS
 
@@ -62,9 +64,18 @@ it under the same terms as Perl itself
 
 =head1 ACKNOWLEDGMENTS
 
-Claes Jakobsson <claesjac at cpan dot org> - I stole and adapted pieces of docs and API design from JavaScript.pm
-Brian Hammond <brain @ fictorial dot com> - For salvaging the code of V8.pm from a message board (which i took some code and the idea from)
-The hacker who wrote V8.pm and posted it on the message board (http://d.hatena.ne.jp/dayflower/20080905/1220592409)
-All the fine people at #perl@freenode.org for helping me write this module
+=over
 
+=item Claes Jakobsson <claesjac at cpan dot org>
+I stole and adapted pieces of docs and API design from JavaScript.pm
+
+=item Brian Hammond <brain @ fictorial dot com>
+For salvaging the code of V8.pm from a message board (which i took some code and the idea from)
+
+=item The hacker who wrote V8.pm and posted it on the message board (http://d.hatena.ne.jp/dayflower/20080905/1220592409)
+
+=item All the fine people at #perl@freenode.org for helping me write this module
+
+=back
+=cut
 
